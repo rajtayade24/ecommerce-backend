6f3cdfb1e429ff2fb988aca0b66a70517083e21f
rajtayade24 <rajtayade2492006@gmail.com>
2026-01-01 23:27:54 +0530

    ignore prod secrets file

--- src/main/java/com/projects/ecommerce/EcommerceApplication.java
+++ src/main/java/com/projects/ecommerce/EcommerceApplication.java
@@ -1,11 +1,24 @@
 package com.projects.ecommerce;
 
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class EcommerceApplication {
 
+    @Autowired
+    private Environment env;  // Inject Spring Environment
+
+    @PostConstruct
+    public void showEnv() {
+        System.out.println("Active Profiles: " + Arrays.toString(env.getActiveProfiles()));
+        System.out.println("DB URL: " + env.getProperty("DB_URL"));
+    }
 	public static void main(String[] args) {
 		SpringApplication.run(EcommerceApplication.class, args);
 	}
